d2ba74a06c46a7292ea8dafdf840081efdda1c65
lihao125 <131753433+lihao125@users.noreply.github.com>
2023-05-17 01:45:13 +0800

    Update userRegController.py

--- BookingSYS/Controller/userRegController.py
+++ BookingSYS/Controller/userRegController.py
@@ -1,38 +1,16 @@
-import sqlite3
-from PyQt5.QtWidgets import QMessageBox
-
+import sys
+sys.path.append( './Entity' )
+from account import Account
 class userRegController:
-    def process_registration(self, dialog, id, username, password, confirm_password):
-        if password == confirm_password:
-            # Connect to thedatabase
-            conn = sqlite3.connect('SilverVillageUserAcc.db')
-            cursor = conn.cursor()
-            try:
-                # Check if ID already exists
-                cursor.execute("SELECT * FROM account WHERE userID=?", (id,))
-                if cursor.fetchone():
-                    QMessageBox.critical(dialog, 'Error', 'ID already exists. Please enter a new ID.')
-                else:
-                    # Check if username already exists
-                    cursor.execute("SELECT * FROM account WHERE userName=?", (username,))
-                    if cursor.fetchone():
-                        QMessageBox.critical(dialog, 'Error', 'Username already exists.')
-                    else:
-                        # Insert registration details into the database
-                        cursor.execute(
-                            "INSERT INTO account (userID, userName, password, permission) VALUES (?, ?, ?, ?)",
-                            (id, username, password, 'customer'))
-                        conn.commit()
+    def process_registration(self, stackedWidget, dialog, id, username, password, confirm_password):
+        print(1)
+        self.stackedWidget=stackedWidget
+        Account().process_registration(self.stackedWidget, dialog, id, username, password, confirm_password)
 
-                        # Registration successful
-                        QMessageBox.information(dialog, 'Success', 'Registration successful.')
-                        dialog.accept()
-            except Exception as e:
-                # Handle database errors
-                QMessageBox.critical(dialog, 'Error', 'An error occurred during registration: {}'.format(str(e)))
-        else:
-            # Passwords do not match
-            QMessageBox.critical(dialog, 'Error', 'Passwords do not match.')
+
+
+
+
 
 
 
