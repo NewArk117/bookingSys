1563923296b5c590bbc4c95276f5a5e9f10c0940
lihao125 <131753433+lihao125@users.noreply.github.com>
2023-05-20 01:14:25 +0800

    Update purchaseFoodUI.py

--- BookingSYS/Boundary/purchaseFoodUI.py
+++ BookingSYS/Boundary/purchaseFoodUI.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import QWidget, QTableWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QTableWidgetItem, \
     QSpinBox, QMessageBox
 from PyQt5.QtCore import Qt
-import sqlite3
+from custFnBController import FoodController
+from ticController import TicketController
+from FBController import PurchaseFoodController
+
 
 class purchaseFoodUI(QWidget):
     def __init__(self, stackedWidget):
@@ -40,22 +43,19 @@ class purchaseFoodUI(QWidget):
         vbox.setContentsMargins(20, 20, 20, 20)
 
         self.setLayout(vbox)
+        self.controller = PurchaseFoodController()
 
         self.purchase_button.clicked.connect(self.purchase)
         self.back_button.clicked.connect(self.back)
         self.show_food()
-        self.viewData()
+
 
     def setID(self, userID):
         self.userID = userID
-        print("Receieved", self.userID)
-
+        self.viewData(self.userID)
 
     def show_food(self):
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM food')
-        food_data = cursor.fetchall()
+        food_data = FoodController.get_food(self)
         self.food_table.setRowCount(len(food_data))
         for row, data in enumerate(food_data):
             food_name, price = data[0], data[1]
@@ -65,38 +65,31 @@ class purchaseFoodUI(QWidget):
             item_quantity.setMinimum(0)
             item_quantity.setMaximum(10)
             item_quantity.setValue(0)
-            item_quantity.valueChanged.connect(
-                self.update_total_price)  # Connect valueChanged signal to update_total_price
+            item_quantity.valueChanged.connect(self.update_total_price)
             item_total = QTableWidgetItem('0.00')
             self.food_table.setItem(row, 0, item_name)
             self.food_table.setItem(row, 1, item_price)
             self.food_table.setCellWidget(row, 2, item_quantity)
             self.food_table.setItem(row, 3, item_total)
 
-        conn.close()
 
-    def viewData(self):
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-        sql = 'SELECT ticket_ID, movieName, showtime, date FROM ticket WHERE userID = ?'
-        data = ('customerTest',)
-        cursor.execute(sql, data)
-        ticket_data = cursor.fetchall()
+
+    def viewData(self, userID):
+        self.userID = userID
+        ticket_data = TicketController.get_tickets(self.userID)
         self.movie_table.setRowCount(len(ticket_data))
         for row, data in enumerate(ticket_data):
             ticket_id, movie_name, showtime, date = data
             item_ticket = QTableWidgetItem(str(ticket_id))
             item_movie = QTableWidgetItem(movie_name)
-            item_time = QTableWidgetItem(str(showtime))  # Convert showtime to string
+            item_time = QTableWidgetItem(str(showtime))
             item_date = QTableWidgetItem(date)
             self.movie_table.setItem(row, 0, item_ticket)
             self.movie_table.setItem(row, 1, item_movie)
             self.movie_table.setItem(row, 2, item_time)
             self.movie_table.setItem(row, 3, item_date)
-        conn.close()
 
     def purchase(self):
-        
         total_price = 0.0
         flag = True
         order_list = []
@@ -117,6 +110,7 @@ class purchaseFoodUI(QWidget):
             QMessageBox.warning(self, 'No Movie Selected', 'Please select which movie you want to buy F&B for.')
             return
 
+
         for row in range(self.food_table.rowCount()):
             item_quantity = self.food_table.cellWidget(row, 2)
             item_price = float(self.food_table.item(row, 1).text())
@@ -125,7 +119,6 @@ class purchaseFoodUI(QWidget):
             self.food_table.item(row, 3).setText('{:.2f}'.format(total))
             total_price += total
 
-        # Create and display the confirmation dialog
         confirmation_text = "Food Order:\n"
         for food_name, quantity in order_list:
             confirmation_text += f"- {food_name}: {quantity}\n"
@@ -152,28 +145,9 @@ class purchaseFoodUI(QWidget):
         result = confirm_dialog.exec()
 
         if result == QMessageBox.Yes:
-            # save the data to the database
-            conn = sqlite3.connect('SilverVillageUserAcc.db')
-            cursor = conn.cursor()
-
             ticket_id_item = self.movie_table.item(selected_row, 0)
-            ticket_id = int(ticket_id_item.text())  # Convert ticket number to integer
-
-            # Insert the order into the food_orders table
-            sql = 'INSERT INTO food_orders (user_id, ticket_id) VALUES (?, ?)'
-            data = ('customerTest', ticket_id)
-            cursor.execute(sql, data)
-            order_id = cursor.lastrowid
-
-            for food_name, quantity in order_list:
-                # Insert the food order into the food_order_items table
-                sql = 'INSERT INTO food_order_items (order_id, food_name, quantity) VALUES (?, ?, ?)'
-                data = (order_id, food_name, quantity)
-                cursor.execute(sql, data)
-
-            conn.commit()
-            conn.close()
-
+            ticket_id = int(ticket_id_item.text())
+            self.controller.save_food_order(self.userID, ticket_id, order_list)
             QMessageBox.information(self, 'Successful Purchase', 'Your purchase has been successfully processed.')
         else:
             QMessageBox.information(self, 'Purchase Cancelled', 'Your purchase has been cancelled.')
@@ -193,6 +167,16 @@ class purchaseFoodUI(QWidget):
 
     def back(self):
         self.stackedWidget.setCurrentIndex(6)
+
+
+
+
+
+
+
+
+
+
 
 
 
