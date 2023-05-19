5c06ddc714176958fb4cffaec4900f6f71d0b083
lihao125 <131753433+lihao125@users.noreply.github.com>
2023-05-20 01:22:58 +0800

    Update customerInfoUI.py

--- BookingSYS/Boundary/customerInfoUI.py
+++ BookingSYS/Boundary/customerInfoUI.py
@@ -6,6 +6,10 @@ sys.path.append('./Boundary')
 
 from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout, QListWidget, QVBoxLayout, QMessageBox, \
     QListWidgetItem, QDialog, QLineEdit, QDialogButtonBox
+from viewTicPurchasedController import TicketController
+from fnbRefundController import FnbRefundController
+from custAccController import AccountController
+
 
 
 # This class shows the record of buying tickets or food
@@ -43,18 +47,15 @@ class customerInfoUI(QWidget):
 
     def setID(self, userID):
         self.userID = userID
-        print("Receieved", self.userID)
 
     def show_ticket_record_window(self):
         widget = self.stackedWidget.widget(20)
         widget.setID(self.userID)
-        print("Sending", self.userID)
         self.stackedWidget.setCurrentIndex(20)
 
     def show_fnb_record_window(self):
         widget = self.stackedWidget.widget(21)
         widget.setID(self.userID)
-        print("Sending", self.userID)
         self.stackedWidget.setCurrentIndex(21)
 
     def show_account_info(self):
@@ -105,25 +106,8 @@ class ticketPurchasedUI(QWidget):
         self.stackedWidget.setCurrentIndex(8)
 
     def viewData(self):
-        print("ID here", self.userID)
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-        sql = 'SELECT ticket_ID, movieName ,hallName ,seat_No ,showtime ,date ,type ,price FROM ticket WHERE userID = ?'
-        data = (self.userID,)
-        cursor.execute(sql, data)
-        ticket_data = cursor.fetchall()
-        ticket_strings = []
-        for row in ticket_data:
-            ticket_string = '{:<10}\t{:<20}\t{:<20}\t{:<20}\t{:<20}\t{:<20}\t{:<10}\t{:<10}'.format(row[0], row[1],
-                                                                                                    row[2], row[3],
-                                                                                                    row[4], row[5],
-                                                                                                    row[6], row[7])
-            ticket_strings.append(ticket_string)
-        self.ticketList.clear()
-        self.ticketList.addItems(ticket_strings)
-
-        conn.commit()
-        conn.close()
+        ticket_controller = TicketController(self)
+        ticket_controller.retrieveUserTickets()
 
     def confirmRefund(self):
         selected_items = self.ticketList.selectedItems()
@@ -152,22 +136,9 @@ class ticketPurchasedUI(QWidget):
 
         result = confirm_dialog.exec_()
         if result == QMessageBox.Yes:
-            self.deleteTicket(ticket_id)
+            ticket_controller = TicketController(self)
+            ticket_controller.deleteTicket(ticket_id)
 
-    def deleteTicket(self, ticket_id):
-
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-
-        sql = 'DELETE FROM ticket WHERE ticket_ID = ?'
-        data = (ticket_id,)
-        cursor.execute(sql, data)
-
-        conn.commit()
-        conn.close()
-
-        QMessageBox.information(self, 'Success', 'Ticket refunded successfully.')
-        self.viewData()
 
 class fnbRefundUI(QWidget):
     def __init__(self, stackedWidget):
@@ -178,7 +149,6 @@ class fnbRefundUI(QWidget):
         self.refund_confirmation_button = QPushButton('Refund Confirmation')
         self.back_button = QPushButton('Back')
 
-        self.refund_confirmation_button.clicked.connect(self.confirm_refund)
         self.back_button.clicked.connect(self.go_back)
 
         self.food_list = QListWidget()
@@ -194,67 +164,18 @@ class fnbRefundUI(QWidget):
         button_layout.addWidget(self.refund_confirmation_button)
         layout.addLayout(button_layout)
 
+        self.controller = FnbRefundController(self)
+        self.refund_confirmation_button.clicked.connect(self.controller.confirm_refund)
+
     def set_order_id(self, order_id):
         self.order_id = order_id
         self.order_id_label.setText("Order ID: {}".format(order_id))
-        self.show_food_list(order_id)  # Update the food list when order ID is set
+        self.controller.show_food_list(order_id)  # Update the food list when order ID is set
 
     def go_back(self):
         self.stackedWidget.setCurrentIndex(21)
 
-    def confirm_refund(self):
-        selected_items = self.food_list.selectedItems()
 
-        if len(selected_items) > 0:
-
-            conn = sqlite3.connect('SilverVillageUserAcc.db')
-            cursor = conn.cursor()
-
-            for item in selected_items:
-                food_info = item.text().split('\n')
-                food_name = food_info[0].split(':')[1].strip()
-                quantity = int(food_info[1].split(':')[1].strip())
-
-                sql = '''
-                DELETE FROM food_order_items
-                WHERE order_id = ? AND food_name = ? AND quantity = ?
-                '''
-
-                data = (self.order_id, food_name, quantity)
-                cursor.execute(sql, data)
-
-            conn.commit()
-            conn.close()
-
-            self.show_food_list(self.order_id)# Update the food list after deletion
-            self.stackedWidget.setCurrentIndex(8)
-
-
-    def show_food_list(self, order_id):
-        # Clear the food list
-        self.food_list.clear()
-
-       # Fetch and display the food items for the given order ID
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-
-        sql = '''
-        SELECT food_name, quantity
-        FROM food_order_items
-        WHERE order_id = ?
-        '''
-
-        data = (order_id,)
-        cursor.execute(sql, data)
-        food_items = cursor.fetchall()
-
-        conn.close()
-
-        for item in food_items:
-            food_name, quantity = item
-            food_info = f"Food Name: {food_name}\nQuantity: {quantity}\n"
-            list_item = QListWidgetItem(food_info)
-            self.food_list.addItem(list_item)
 
 
 
@@ -391,29 +312,15 @@ class AccountInfoUI(QWidget):
         self.change_password_button.clicked.connect(self.change_password)
         self.back_button.clicked.connect(self.go_back)
 
+        self.controller = AccountController()
+
     def setID(self, userID):
         self.userID = userID
         self.show_account_info(self.userID)
 
     def show_account_info(self, userID):
-
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-
-        # Get username from database based on userid
-        sql = '''
-        SELECT userName
-        FROM account
-        WHERE userID = ?
-        '''
-        data = (userID,)
-        cursor.execute(sql, data)
-        result = cursor.fetchone()
-
-        conn.close()
-
-        if result:
-            username = result[0]
+        username = self.controller.get_username(userID)
+        if username:
             self.account_label.setText(f'Account ID: {userID}\nAccount Name: {username}')
         else:
             self.account_label.setText('Account Name: N/A')
@@ -444,28 +351,11 @@ class AccountInfoUI(QWidget):
             new_userID = user_id_input.text()
             new_username = username_input.text()
 
-
-            conn = sqlite3.connect('SilverVillageUserAcc.db')
-            cursor = conn.cursor()
-
-            # Update userID and userName in the database
-            sql = '''
-            UPDATE account
-            SET userID = ?,
-                userName = ?
-            WHERE userID = ?
-            '''
-            data = (new_userID, new_username, self.userID)
-            cursor.execute(sql, data)
-            conn.commit()
-
-            conn.close()
+            self.controller.update_account_info(self.userID, new_userID, new_username)
 
             self.userID = new_userID
             self.show_account_info(self.userID)
             QMessageBox.information(self, 'Success', 'Account information updated successfully.')
-
-
 
     def change_password(self):
         dialog = QDialog(self)
@@ -502,24 +392,10 @@ class AccountInfoUI(QWidget):
         dialog.exec_()
 
     def validate_and_change_password(self, dialog, old_password, new_password, confirm_password):
-        if old_password == self.get_password_from_database():
+        if old_password == self.controller.get_password(self.userID):
             if new_password == confirm_password:
                 try:
-                    conn = sqlite3.connect('SilverVillageUserAcc.db')
-                    cursor = conn.cursor()
-
-                    # update the password in the database
-                    sql = '''
-                    UPDATE account
-                    SET password = ?
-                    WHERE userID = ?
-                    '''
-                    data = (new_password, self.userID)
-                    cursor.execute(sql, data)
-                    conn.commit()
-
-                    conn.close()
-
+                    self.controller.update_password(self.userID, new_password)
                     QMessageBox.information(self, 'Success', 'Password changed successfully.')
                 except Exception as e:
                     print(f"An error occurred while changing password: {str(e)}")
@@ -530,31 +406,7 @@ class AccountInfoUI(QWidget):
 
         dialog.accept()
 
-    def get_password_from_database(self):
-
-        conn = sqlite3.connect('SilverVillageUserAcc.db')
-        cursor = conn.cursor()
-
-        # Get password from database based on user id
-        sql = '''
-        SELECT password
-        FROM account
-        WHERE userID = ?
-        '''
-        data = (self.userID,)
-        cursor.execute(sql, data)
-        result = cursor.fetchone()
-
-        conn.close()
-
-        if result:
-            return result[0]
-
-
     def go_back(self):
         self.stackedWidget.setCurrentIndex(8)
-
-
-
 
 
