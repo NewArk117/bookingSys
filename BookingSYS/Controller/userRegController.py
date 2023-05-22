ae29865d23ce77b34eb816a95da5bd0ef4c79cdd
lihao125 <131753433+lihao125@users.noreply.github.com>
2023-05-22 20:02:24 +0800

    Update userRegController.py

--- BookingSYS/Controller/userRegController.py
+++ BookingSYS/Controller/userRegController.py
@@ -2,10 +2,12 @@ import sys
 sys.path.append( './Entity' )
 from account import Account
 class userRegController:
-    def process_registration(self, stackedWidget, dialog, id, username, password, confirm_password):
+    def process_registration(self, stackedWidget, dialog, id, password, confirm_password):
         print(1)
         self.stackedWidget=stackedWidget
-        Account().process_registration(self.stackedWidget, dialog, id, username, password, confirm_password)
+        Account().process_registration(self.stackedWidget, dialog, id, password, confirm_password)
+
+
 
 
 
