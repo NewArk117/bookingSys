c24d7e1fea7ed8a1e878bdf049c430de6e01c278
lihao125 <131753433+lihao125@users.noreply.github.com>
2023-05-22 19:52:20 +0800

    Update purchaseFoodUI.py

--- BookingSYS/Boundary/purchaseFoodUI.py
+++ BookingSYS/Boundary/purchaseFoodUI.py
@@ -17,8 +17,8 @@ class purchaseFoodUI(QWidget):
         self.resize(600, 300)
 
         self.food_table = QTableWidget(self)
-        self.food_table.setColumnCount(4)
-        self.food_table.setHorizontalHeaderLabels(['Food', 'Price', 'Quantity', 'Total'])
+        self.food_table.setColumnCount(5)
+        self.food_table.setHorizontalHeaderLabels(['Food', 'Price', 'Quantity', "Purchase", 'Total'])
 
         self.movie_table = QTableWidget(self)
         self.movie_table.setColumnCount(4)
@@ -45,6 +45,8 @@ class purchaseFoodUI(QWidget):
         self.setLayout(vbox)
         self.controller = PurchaseFoodController()
 
+        self.refresh_ui()
+
         self.purchase_button.clicked.connect(self.purchase)
         self.back_button.clicked.connect(self.back)
         self.show_food()
@@ -58,21 +60,28 @@ class purchaseFoodUI(QWidget):
         food_data = FoodController.get_food(self)
         self.food_table.setRowCount(len(food_data))
         for row, data in enumerate(food_data):
-            food_name, price = data[0], data[1]
+            food_name, price, quantity, available = data[0], data[1], data[2], data[3]
             item_name = QTableWidgetItem(food_name)
             item_price = QTableWidgetItem(str(price))
-            item_quantity = QSpinBox()
-            item_quantity.setMinimum(0)
-            item_quantity.setMaximum(10)
-            item_quantity.setValue(0)
-            item_quantity.valueChanged.connect(self.update_total_price)
+            item_quantity = QTableWidgetItem(str(quantity))
+            item_purchase = QSpinBox()
+            item_purchase.setMinimum(0)
+            item_purchase.setMaximum(quantity)
+            item_purchase.setValue(0)
+            item_purchase.valueChanged.connect(self.update_total_price)
             item_total = QTableWidgetItem('0.00')
             self.food_table.setItem(row, 0, item_name)
             self.food_table.setItem(row, 1, item_price)
-            self.food_table.setCellWidget(row, 2, item_quantity)
-            self.food_table.setItem(row, 3, item_total)
+            self.food_table.setItem(row, 2, item_quantity)
+            self.food_table.setCellWidget(row, 3, item_purchase)
+            self.food_table.setItem(row, 4, item_total)
 
-
+            if available == 0:
+                item_name.setFlags(item_name.flags() & ~Qt.ItemIsSelectable)
+                item_price.setFlags(item_price.flags() & ~Qt.ItemIsSelectable)
+                item_quantity.setFlags(item_quantity.flags() & ~Qt.ItemIsSelectable)
+                item_purchase.setEnabled(False)
+                item_total.setFlags(item_total.flags() & ~Qt.ItemIsSelectable)
 
     def viewData(self, userID):
         self.userID = userID
@@ -95,8 +104,8 @@ class purchaseFoodUI(QWidget):
         order_list = []
 
         for row in range(self.food_table.rowCount()):
-            item_quantity = self.food_table.cellWidget(row, 2)
-            quantity = item_quantity.value()
+            item_purchase = self.food_table.cellWidget(row, 3)
+            quantity = item_purchase.value()
             if quantity != 0:
                 flag = False
                 food_name = self.food_table.item(row, 0).text()
@@ -112,11 +121,11 @@ class purchaseFoodUI(QWidget):
 
 
         for row in range(self.food_table.rowCount()):
-            item_quantity = self.food_table.cellWidget(row, 2)
+            item_purchase = self.food_table.cellWidget(row, 3)
             item_price = float(self.food_table.item(row, 1).text())
-            quantity = item_quantity.value()
+            quantity = item_purchase.value()
             total = item_price * quantity
-            self.food_table.item(row, 3).setText('{:.2f}'.format(total))
+            self.food_table.item(row, 4).setText('{:.2f}'.format(total))
             total_price += total
 
         confirmation_text = "Food Order:\n"
@@ -148,32 +157,30 @@ class purchaseFoodUI(QWidget):
             ticket_id_item = self.movie_table.item(selected_row, 0)
             ticket_id = int(ticket_id_item.text())
             self.controller.save_food_order(self.userID, ticket_id, order_list)
+            for food_name, quantity in order_list:
+                self.controller.update_fnb(food_name, quantity)
             QMessageBox.information(self, 'Successful Purchase', 'Your purchase has been successfully processed.')
+            self.refresh_ui()
         else:
             QMessageBox.information(self, 'Purchase Cancelled', 'Your purchase has been cancelled.')
-
-
 
     def update_total_price(self):
         total_price = 0.0
         for row in range(self.food_table.rowCount()):
-            item_quantity = self.food_table.cellWidget(row, 2)
+            item_quantity = self.food_table.cellWidget(row, 3)
             item_price = float(self.food_table.item(row, 1).text())
             quantity = item_quantity.value()
             total = item_price * quantity
-            self.food_table.item(row, 3).setText('{:.2f}'.format(total))
+            self.food_table.item(row, 4).setText('{:.2f}'.format(total))
             total_price += total
         return total_price
 
     def back(self):
         self.stackedWidget.setCurrentIndex(6)
 
-
-
-
-
-
-
+    def refresh_ui(self):
+        self.show_food()
+        self.viewData(self.userID)
 
 
 
