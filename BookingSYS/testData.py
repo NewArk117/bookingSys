3162a1187fc427f0a1aefebb33f80933f0979a3e
chanzs415 <chanzishen@gmail.com>
2023-05-23 23:47:30 +0800

    populated food and ticket

--- BookingSYS/testData.py
+++ BookingSYS/testData.py
@@ -244,8 +244,8 @@ def addMovie(name, genre, hallname,cursor):
             data3 = (0, hall, time, date)
             cursor.execute(sql3, data3)
 
-hallList = ["Hall-1", "Hall-2", "Hall-3", "Hall-4", "Hall-5"]
-movieList = [["Saw", "Thriller","Hall-1"], ["James Bond","Action","Hall-2" ],["Toy Story", "Family", "Hall-3"], ["Jumanji", "Adventure", "Hall-4"],  ["Avengers", "Action", "Hall-5"]]
+hallList = ["Hall-1", "Hall-2", "Hall-3", "Hall-4", "Hall-5", "Hall-6", "Hall-7", "Hall-8", "Hall-9", "Hall-10"]
+movieList = [["Saw", "Thriller","Hall-1"], ["James Bond","Action","Hall-2" ],["Toy Story", "Family", "Hall-3"], ["Jumanji", "Adventure", "Hall-4"],  ["Avengers", "Action", "Hall-5"], ["Spiderman", "Action", "Hall-6"], ["Titanic", "Love", "Hall-7"], ["Transformers", "Sci-Fi","Hall-8"], ["E.T.", "Sci-Fi","Hall-9"], ["Romeo","Love","Hall-10"]]
 
 for x in hallList:
     addHall(x, 5, 6, cursor)    
@@ -263,7 +263,15 @@ def addFood( name, price, quantity, cursor):
 
 addFood("Burger", 5.2, 10 , cursor)
 addFood("Coke", 2.5, 10, cursor)
-addFood("Popcorn", 6.0, 10, cursor)
+addFood("Popcorn", 7.0, 10, cursor)
+addFood("HotDog", 8.0, 10, cursor)
+addFood("Cake", 9.0, 10, cursor)
+addFood("Fries", 8.0, 10, cursor)
+addFood("Candy", 7.0, 10, cursor)
+addFood("Chocolate", 6.0, 10, cursor)
+addFood("Sprite", 7.0, 10, cursor)
+addFood("Nachos", 8.0, 10, cursor)
+
 
 def addTicketType(name, price, cursor):
     sql = "INSERT INTO ticketType (type, price, isAvailable) VALUES (?, ?, ?)"
@@ -285,10 +293,97 @@ def purchaseTic( movieName, genre, hallName, selectedDate, selectedTime,ticCount
             data2 = (userID, movieName, hallName, x[0], selectedTime, selectedDate, x[1], x[2])
             cursor.execute(sql2, data2)
 
-purchaseTic("Saw", "Thriller" ,"Hall-1" ,"2023-05-01", 1330 ,7,[['B-4', 'Adult', 12], ['C-4', 'Adult', 12], ['D-4', 'Adult', 12], ['D-5', 'Adult', 12], ['C-6', 'Adult', 12], ['C-3', 'Adult', 12], ['B-3', 'Adult', 12]] ,84 ,"customerTest", cursor)
-purchaseTic("James Bond" ,"Action", "Hall-2",  "2023-06-20", 1730, 7, [['B-4', 'Adult', 12], ['C-4', 'Child', 10], ['C-2', 'Adult', 12], ['B-2', 'Adult', 12], ['B-3', 'Child', 10], ['D-3', 'Child', 10], ['E-4', 'Senior', 9]], 75 ,"customerTest",cursor)
-    #conn.commit()
-    #conn.close()
+
+purchaseTic("Saw", "Thriller" ,"Hall-1" ,"2023-05-01", 1330 ,7,[['B-4', 'Adult', 12], ['C-4', 'Adult', 12], ['D-4', 'Adult', 12], ['D-5', 'Adult', 12], ['C-6', 'Adult', 12], ['C-3', 'Adult', 12], ['B-3', 'Adult', 12]] ,84 ,"customer", cursor)
+purchaseTic("James Bond" ,"Action", "Hall-2",  "2023-06-20", 1730, 7, [['B-4', 'Adult', 12], ['C-4', 'Child', 10], ['C-2', 'Adult', 12], ['B-2', 'Adult', 12], ['B-3', 'Child', 10], ['D-3', 'Child', 10], ['E-4', 'Senior', 9]], 75 ,"customer",cursor)
+
+def generate_random_transactions(cursor):
+    customers = [f"cust{i}" for i in range(100)]
+    
+    for _ in range(100):
+        start_date = datetime.date(2023, 5, 1)
+        #startDate = date.today()
+        delta = datetime.timedelta(days=1)
+        end_date = datetime.date(2023, 6 , 30)
+        #currentDate = startDate
+        showtimes = ["1330", "1530", "1730", "1930", "2130"]
+        customer_id = random.choice(customers)
+        movie = random.choice(movieList)
+        movie_name, genre, hall_name = movie
+        
+        selected_date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
+        selected_time = int(random.choice(showtimes))
+        
+        tic_count = random.randint(1, 5)
+        seat_list = []
+        for _ in range(tic_count):
+            hall = random.choice(hallList)
+            seat_row = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
+            seat_col = random.randint(1, 6)
+            seat = f"{seat_row}-{seat_col}"
+            seat_type = random.choice(["Adult", "Child"])
+            seat_price = random.randint(10, 20)
+            seat_list.append([seat, seat_type, seat_price])
+        
+        total_cost = sum([seat[2] for seat in seat_list])
+        
+        purchaseTic(movie_name, genre, hall_name, selected_date, selected_time, tic_count, seat_list, total_cost, customer_id, cursor)
+
+generate_random_transactions(cursor)
+
+
+foodList = [
+    ("Burger", 5.2, 10),
+    ("Coke", 2.5, 10),
+    ("Popcorn", 7.0, 10),
+    ("HotDog", 8.0, 10),
+    ("Cake", 9.0, 10),
+    ("Fries", 8.0, 10),
+    ("Candy", 7.0, 10),
+    ("Chocolate", 6.0, 10),
+    ("Sprite", 7.0, 10),
+    ("Nachos", 8.0, 10)
+]
+
+def populate_food_orders(cursor):
+    order_count = 100
+    customer_count = 100
+    ticket_count = 100
+    
+    for _ in range(order_count):
+        order_id = None  # Auto-incremented primary key
+        
+        # Select a random customer
+        user_id = f"cust{random.randint(0, customer_count-1)}"
+        
+        # Select a random ticket
+        ticket_id = random.randint(1, ticket_count)
+        
+        # Insert the record into food_orders table
+        cursor.execute("INSERT INTO food_orders (user_id, ticket_id) VALUES (?, ?)", (user_id, ticket_id))
+        
+        # Get the auto-generated order_id
+        order_id = cursor.lastrowid
+        
+        # Randomly generate the number of food items for the order
+        num_items = random.randint(1, 5)
+        
+        # Select random food items and quantities
+        food_items = random.sample(foodList, num_items)
+        quantities = [random.randint(1, 5) for _ in range(num_items)]
+        
+        # Insert the food order items into food_order_items table
+        for i in range(num_items):
+            food_name, _, _ = food_items[i]
+            quantity = quantities[i]
+            
+            cursor.execute("INSERT INTO food_order_items (order_id, food_name, quantity) VALUES (?, ?, ?)",
+                           (order_id, food_name, quantity))
+
+populate_food_orders(cursor)
+
+
+# Populating food_orders and food_order_items tables
 
 #sql = "INSERT INTO hall (capacity, isAccessible) VALUES (?, ?)"
 #data = (150,0)
